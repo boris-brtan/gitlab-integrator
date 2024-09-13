a503f5284ab93370f61ff435189b6c298268290a
c.manhart <c.manhart@adito.de>
2024-09-13 11:03:10 +0200

    - support MR title & description - support regex configuration for search & replacement - support custom placing of the replacement within the url - change naming to Gitlab integrator (it supports just gitlab)

--- content-script.js
+++ content-script.js
@@ -1,16 +1,32 @@
 (async function enhance() {
-    const { items } = await chrome.storage.sync.get(['items'])
-    for (const { name, prefix, url } of items) {
-        if (location.pathname.includes(name)) {
-            const title = document.querySelector('h1')
-            title.innerHTML = title.innerHTML.replace(
-                /<a.*?>|<\/a>/g,
-                '',
-            ).replace(
-                RegExp(`(${prefix}-\\d+)`, 'g'),
-                `<a href="${url}$1" title="${url}$1">$1</a>`,
-            )
-            break;
-        }
+    const { items } = await chrome.storage.sync.get(['items']);
+    let replacements = [
+        //merge request title
+        document.querySelector('h1')
+    ];
+
+    //merge request description
+    let title = document.getElementsByClassName('description');
+
+    if(title.length) {
+        replacements.push(title[0]);
     }
-})()
+
+    for (const { name, prefix, url } of items) {
+        replacements.forEach(pReplacement => {
+            //setup a regex for the name field
+            const nameReg = new RegExp(name, 'gi');
+
+            //match the current url with the name regex
+            if (nameReg.test(location.href)) {
+                //make it possible to use the replacement on different locations than at the end of the url
+                const replaceUrl = url.includes('$1') ? url : `${url}$1`;
+
+                pReplacement.innerHTML = pReplacement.innerHTML.replace(
+                    RegExp(`(${prefix})`, 'gi'),
+                    `<a href="${replaceUrl}" title="${replaceUrl}">$1</a>`,
+                );
+            }
+        });
+    }
+})();
