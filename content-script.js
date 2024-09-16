c32658e2fbee5714f34304f34e20418971265627
c.manhart <c.manhart@adito.de>
2024-09-16 08:11:20 +0200

    fix comments

--- content-script.js
+++ content-script.js
@@ -1,25 +1,20 @@
 (async function enhance() {
     const { items } = await chrome.storage.sync.get(['items']);
-    let replacements = [
-        //merge request title
-        document.querySelector('h1')
-    ];
-
-    //merge request description
-    let title = document.getElementsByClassName('description');
-
-    if(title.length) {
-        replacements.push(title[0]);
-    }
+    const replacements = [
+        // merge request title
+        document.querySelector('h1'),
+        // merge request description
+        document.querySelector('.description')
+    ].filter(Boolean);
 
     for (const { name, prefix, url } of items) {
         replacements.forEach(pReplacement => {
-            //setup a regex for the name field
+            // setup a regex for the name field
             const nameReg = new RegExp(name, 'gi');
 
-            //match the current url with the name regex
+            // match the current url with the name regex
             if (nameReg.test(location.href)) {
-                //make it possible to use the replacement on different locations than at the end of the url
+                // make it possible to use the replacement on different locations than at the end of the url
                 const replaceUrl = url.includes('$1') ? url : `${url}$1`;
 
                 pReplacement.innerHTML = pReplacement.innerHTML.replace(
